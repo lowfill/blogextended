c25e5b22b1f1016591e605ea55535fca15141cdb
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2011-11-16 22:50:23 -0500

    Fixed code for select group membership for put on assing_to field

--- views/default/groups/groupselector.php
+++ views/default/groups/groupselector.php
@@ -23,17 +23,23 @@ if(get_plugin_setting("groupcontents","blogextended")=="yes"){
 
     $options_values = array(get_loggedin_userid()=>elgg_echo("my:profile"));
 
-    $options['relationship'] = 'member';
-    $options['relationship_guid'] = page_owner();
-    $options['inverse_relationship'] = false;
-    $options['types']=array('group');
-    $options['subtypes']=array(ELGG_ENTITIES_NO_VALUE);
-
-    $objects = elgg_get_entities_from_relationship($options);
-    if(!empty($objects)){
-        foreach($objects as $object){
-            $options_values["{$object->guid}"]=$object->name;
-        }
+    $options = array('relationship'=>'member',
+    				 'relationship_guid'=>page_owner(),
+    				 'inverse_relationship'=>false,
+    				 'types'=>'group',
+    				 'limit'=>50);
+    
+    $count = elgg_get_entities_from_relationship(array_merge($options,array('count'=>true)));
+	if($count>0){
+    for($i=0;$i<$count;$i+=50){
+    	$options['offset']=$i;
+	    $objects = elgg_get_entities_from_relationship($options);
+	    if(!empty($objects)){
+	        foreach($objects as $object){
+	            $options_values["{$object->guid}"]=$object->name;
+	        }
+	    }
+    }
 ?>
 <p><label><?php echo $field_label; ?></label><br />
     <?php echo elgg_view("input/pulldown",array("internalname"=>"container_guid",
